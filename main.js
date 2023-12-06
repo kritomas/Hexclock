b000fd06dd3dbaea0f95c60a8dd94826d2bc673b
kritomas <Unknown>
2023-12-06 08:35:10 +0100

    Added timer support.

--- main.js
+++ main.js
@@ -1,17 +1,45 @@
+var timer = -1;
+var isTimer = false;
+
+function setTimer()
+{
+	timer = parseInt(document.forms["form"]["value"].value);
+	isTimer = true;
+}
+function resetTimer()
+{
+	isTimer = false;
+}
 
 function refreshData()
 {
     const seconds_until_repeat = 1;
- 	var d = new Date()
- 	var h = d.getHours();
- 	var m = d.getMinutes();
- 	var s = d.getSeconds();
+	var color = "#000000"
 
- 	if (h<=9) {h = '0'+h};
- 	if (m<=9) {m = '0'+m};
-	if (s<=9) {s = '0'+s};
+	if (isTimer)
+	{
+		if (timer >= 0)
+		{
+			color = "" + timer;
+			while (color.length < 6) color = "0" + color;
+			color = "#" + color;
+		}
+		--timer
+		if (timer < 0) isTimer = false;
+	}
+	else
+	{
+	 	var d = new Date()
+	 	var h = d.getHours();
+	 	var m = d.getMinutes();
+	 	var s = d.getSeconds();
 
- 	var	color = '#'+h+m+s;
+	 	if (h<=9) h = '0'+h;
+	 	if (m<=9) m = '0'+m;
+		if (s<=9) s = '0'+s;
+
+	 	color = '#'+h+m+s;
+	}
 	document.getElementById("background").style["background-color"] = color;
 	document.getElementById("clock").textContent = color;
 
