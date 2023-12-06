8086c9bc6bb8ef687761082ec6d440c1aaf2f4cb
kritomas <Unknown>
2023-12-06 08:56:20 +0100

    Styles.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var timer = -1;
-var isTimer = false;
+let timer = -1;
+let isTimer = false;
 
 function setTimer()
 {
@@ -14,7 +14,7 @@ function resetTimer()
 function refreshData()
 {
     const seconds_until_repeat = 1;
-	var color = "#000000"
+	let color = "#000000"
 
 	if (isTimer)
 	{
@@ -29,10 +29,10 @@ function refreshData()
 	}
 	else
 	{
-	 	var d = new Date()
-	 	var h = d.getHours();
-	 	var m = d.getMinutes();
-	 	var s = d.getSeconds();
+	 	let d = new Date()
+	 	let h = d.getHours();
+	 	let m = d.getMinutes();
+	 	let s = d.getSeconds();
 
 	 	if (h<=9) h = '0'+h;
 	 	if (m<=9) m = '0'+m;
